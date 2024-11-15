c3e39b08f6c83d463185c8e35e5d852b6a14307d
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-11-15 10:49:10 +0700

    feat: add test log route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test-log', function () {
+    Log::info('this example send log', [
+        'status' => true
+    ]);
+});
